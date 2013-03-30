1802dc31bf3603b7eb7b6c5d98b4d6aa1cd4580b
admiroza <admir.alagic@gmail.com>
2013-03-30 16:35:04 +0100

    fragmenti za tablet

---
+++ playa/src/com/example/playa/ListDetail.java
@@ -0,0 +1,13 @@
+package com.example.playa;
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class ListDetail extends Activity {
+    
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_detail);
+    }
+}
---
+++ playa/src/com/example/playa/Main.java
@@ -0,0 +1,13 @@
+package com.example.playa;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main extends Activity {
+    
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_detail);
+    }
+}
